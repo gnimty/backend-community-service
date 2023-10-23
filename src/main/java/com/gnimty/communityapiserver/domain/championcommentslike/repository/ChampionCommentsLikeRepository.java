19be2a0395fb4d0c137f7d7a986b56f32e8c423f
kariskan <zkfzpf56@naver.com>
2023-10-23 17:00:16 +0900

    feat: 회원으로 챔피언 운용법 좋아요/싫어요 조회하는 메서드 작성

--- src/main/java/com/gnimty/communityapiserver/domain/championcommentslike/repository/ChampionCommentsLikeRepository.java
+++ src/main/java/com/gnimty/communityapiserver/domain/championcommentslike/repository/ChampionCommentsLikeRepository.java
@@ -3,9 +3,11 @@ package com.gnimty.communityapiserver.domain.championcommentslike.repository;
 import com.gnimty.communityapiserver.domain.championcomments.entity.ChampionComments;
 import com.gnimty.communityapiserver.domain.championcommentslike.entity.ChampionCommentsLike;
 import com.gnimty.communityapiserver.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChampionCommentsLikeRepository extends JpaRepository<ChampionCommentsLike, Long> {
 
 	Boolean existsByMemberAndChampionComments(Member member, ChampionComments championComments);
+	Optional<ChampionCommentsLike> findByMemberAndChampionComments(Member member, ChampionComments championComments);
 }
