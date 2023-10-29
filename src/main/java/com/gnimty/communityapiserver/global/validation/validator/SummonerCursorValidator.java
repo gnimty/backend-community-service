bd4c7326fa612845d5c683c2b91a1dafe2b6c018
kariskan <zkfzpf56@naver.com>
2023-10-29 15:58:47 +0900

    feat: RecommendedSummonersRequest에 validation 추가

---
+++ src/main/java/com/gnimty/communityapiserver/global/validation/validator/SummonerCursorValidator.java
@@ -0,0 +1,63 @@
+package com.gnimty.communityapiserver.global.validation.validator;
+
+import com.gnimty.communityapiserver.global.constant.SortBy;
+import com.gnimty.communityapiserver.global.exception.BaseException;
+import com.gnimty.communityapiserver.global.exception.ErrorCode;
+import com.gnimty.communityapiserver.global.validation.annotation.ValidateCursor;
+import java.lang.reflect.Field;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class SummonerCursorValidator implements ConstraintValidator<ValidateCursor, Object> {
+
+	private String sortBy;
+	private String lastSummonerName;
+	private String lastSummonerMmr;
+	private String lastSummonerUpCount;
+
+	@Override
+	public void initialize(ValidateCursor constraintAnnotation) {
+		sortBy = constraintAnnotation.sortBy();
+		lastSummonerName = constraintAnnotation.lastSummonerName();
+		lastSummonerMmr = constraintAnnotation.lastSummonerMmr();
+		lastSummonerUpCount = constraintAnnotation.lastSummonerUpCount();
+	}
+
+	@Override
+	public boolean isValid(Object value, ConstraintValidatorContext context) {
+		SortBy sortByField = getObjectValue(value, sortBy, SortBy.class);
+		String lastSummonerNameField = getObjectValue(value, lastSummonerName, String.class);
+		Long lastSummonerMmrField = getObjectValue(value, lastSummonerMmr, Long.class);
+		Long lastSummonerUpCountField = getObjectValue(value, lastSummonerUpCount, Long.class);
+
+		if (sortByField == null) {
+			throw new BaseException(ErrorCode.CONSTRAINT_VIOLATION);
+		}
+		if (sortByField.equals(SortBy.ATOZ)) {
+			return lastSummonerNameField != null;
+		}
+		if (sortByField.equals(SortBy.TIER)) {
+			return lastSummonerMmrField != null;
+		}
+		return lastSummonerUpCountField != null;
+	}
+
+	private <T> T getObjectValue(Object object, String fieldName, Class<T> type) {
+		Class<?> clazz = object.getClass();
+		Field dateField;
+		try {
+			dateField = clazz.getDeclaredField(fieldName);
+			dateField.setAccessible(true);
+			Object target = dateField.get(object);
+			if (target == null) {
+				return null;
+			}
+			if (!type.isInstance(target)) {
+				throw new BaseException(ErrorCode.CONSTRAINT_VIOLATION);
+			}
+			return type.cast(target);
+		} catch (NoSuchFieldException | IllegalAccessException e) {
+			throw new BaseException(ErrorCode.CONSTRAINT_VIOLATION);
+		}
+	}
+}
