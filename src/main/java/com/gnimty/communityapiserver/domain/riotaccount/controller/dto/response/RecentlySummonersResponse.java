e1b68d216d66d53ea5d198c7a605982c6cd1c153
kariskan <73534426+kariskan@users.noreply.github.com>
2023-11-08 15:03:48 +0900

    Feat/#10 최근 플레이 한 소환사 추천 조회 (#47)
    * feat: 최근 게임한 소환사 response dto 작성
    
    * feat: member id로 대표 소환사 계정 조회 쿼리 작성
    
    * feat: member id로 대표 소환사 계정 조회 쿼리 작성
    
    * feat: 최근 플레이 한 소환사 조회
    
    - WebClient를 이용해 최근 20게임 전적 조회
    - chatService에서 채팅방 목록 조회
    - 겹치면서, 내가 좋아요 하지 않은 소환사만 필터링

---
+++ src/main/java/com/gnimty/communityapiserver/domain/riotaccount/controller/dto/response/RecentlySummonersResponse.java
@@ -0,0 +1,24 @@
+package com.gnimty.communityapiserver.domain.riotaccount.controller.dto.response;
+
+import com.gnimty.communityapiserver.domain.riotaccount.service.dto.response.RecentlySummonersServiceResponse;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecentlySummonersResponse {
+
+	private List<RecentlySummonersEntry> recentlySummoners;
+
+	public static RecentlySummonersResponse from(RecentlySummonersServiceResponse response) {
+		return RecentlySummonersResponse.builder()
+				.recentlySummoners(response.getRecentlySummoners())
+				.build();
+	}
+
+}
---
+++ src/main/java/com/gnimty/communityapiserver/domain/riotaccount/service/dto/response/RecentlySummonersServiceResponse.java
@@ -0,0 +1,17 @@
+package com.gnimty.communityapiserver.domain.riotaccount.service.dto.response;
+
+import com.gnimty.communityapiserver.domain.riotaccount.controller.dto.response.RecentlySummonersEntry;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecentlySummonersServiceResponse {
+
+	private List<RecentlySummonersEntry> recentlySummoners;
+}
