a0e5b91f48178a066302e0a55c9ff5e8fe0b3e00
kariskan <zkfzpf56@naver.com>
2023-11-10 17:21:44 +0900

    fix: dto에 validation 추가

--- src/main/java/com/gnimty/communityapiserver/domain/member/controller/dto/request/SendEmailRequest.java
+++ src/main/java/com/gnimty/communityapiserver/domain/member/controller/dto/request/SendEmailRequest.java
@@ -1,6 +1,8 @@
 package com.gnimty.communityapiserver.domain.member.controller.dto.request;
 
 import com.gnimty.communityapiserver.domain.member.service.dto.request.SendEmailServiceRequest;
+import com.gnimty.communityapiserver.global.exception.ErrorCode.ErrorMessage;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SendEmailRequest {
 
+	@NotNull(message = ErrorMessage.INVALID_INPUT_VALUE)
 	private String email;
 
 	public SendEmailServiceRequest toServiceRequest() {
