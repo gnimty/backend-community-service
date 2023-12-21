d4acd44deda1db6dd7d33db26985dcfe6e941b10
kariskan <zkfzpf56@naver.com>
2023-12-21 16:22:03 +0900

    feat: 챔피언 운용법 신고 entity 작성

---
+++ src/main/java/com/gnimty/communityapiserver/domain/championcommentsreport/entity/ChampionCommentsReport.java
@@ -0,0 +1,60 @@
+package com.gnimty.communityapiserver.domain.championcommentsreport.entity;
+
+import com.gnimty.communityapiserver.domain.base.entity.BaseEntity;
+import com.gnimty.communityapiserver.domain.championcomments.entity.ChampionComments;
+import com.gnimty.communityapiserver.domain.member.entity.Member;
+import com.gnimty.communityapiserver.global.constant.ReportType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "champion_comments_report")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChampionCommentsReport extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "champion_comments_report_id", columnDefinition = "BIGINT", unique = true, nullable = false)
+	private Long id;
+
+	@NotNull
+	@Column(name = "report_type", columnDefinition = "VARCHAR(100)")
+	private ReportType reportType;
+
+	@Column(name = "report_comment", columnDefinition = "VARCHAR(100)")
+	private String reportComment;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "champion_comments_id", nullable = false)
+	private ChampionComments championComments;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@Builder
+	public ChampionCommentsReport(
+		ReportType reportType,
+		String reportComment,
+		ChampionComments championComments,
+		Member member
+	) {
+		this.reportType = reportType;
+		this.reportComment = reportComment;
+		this.championComments = championComments;
+		this.member = member;
+	}
+}
