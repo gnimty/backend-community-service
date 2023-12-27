c0d87cf637854a12e7dec740787b0669ef63c5ae
kariskan <zkfzpf56@naver.com>
2023-12-27 20:18:44 +0900

    feat: builder 및 생성자 추가

--- src/main/java/com/gnimty/communityapiserver/domain/memberlike/controller/dto/request/MemberLikeRequest.java
+++ src/main/java/com/gnimty/communityapiserver/domain/memberlike/controller/dto/request/MemberLikeRequest.java
@@ -3,9 +3,15 @@ package com.gnimty.communityapiserver.domain.memberlike.controller.dto.request;
 import com.gnimty.communityapiserver.domain.memberlike.service.dto.request.MemberLikeServiceRequest;
 import com.gnimty.communityapiserver.global.exception.ErrorCode.ErrorMessage;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberLikeRequest {
 
 	@NotNull(message = ErrorMessage.INVALID_INPUT_VALUE)
