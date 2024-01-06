c2d03211a6757649c62f4cf9b79512834de20f53
kariskan <zkfzpf56@naver.com>
2024-01-06 20:03:39 +0900

    feat: enum Schema 작성

--- src/main/java/com/gnimty/communityapiserver/global/constant/CommentsType.java
+++ src/main/java/com/gnimty/communityapiserver/global/constant/CommentsType.java
@@ -3,8 +3,16 @@ package com.gnimty.communityapiserver.global.constant;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.gnimty.communityapiserver.global.exception.BaseException;
 import com.gnimty.communityapiserver.global.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.stream.Stream;
 
+@Schema(
+	enumAsRef = true,
+	description = """
+		QUESTION - 질문
+		TIP - 팁
+		"""
+)
 public enum CommentsType {
 
 	QUESTION,
--- src/main/java/com/gnimty/communityapiserver/global/constant/DayOfWeek.java
+++ src/main/java/com/gnimty/communityapiserver/global/constant/DayOfWeek.java
@@ -3,9 +3,22 @@ package com.gnimty.communityapiserver.global.constant;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.gnimty.communityapiserver.global.exception.BaseException;
 import com.gnimty.communityapiserver.global.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.stream.Stream;
 import lombok.Getter;
 
+@Schema(
+	enumAsRef = true,
+	description = """
+		SUNDAY - 일
+		MONDAY - 월
+		TUESDAY - 화
+		WEDNESDAY - 수
+		THURSDAY - 목
+		FRIDAY - 금
+		SATURDAY - 토
+		"""
+)
 @Getter
 public enum DayOfWeek {
 
