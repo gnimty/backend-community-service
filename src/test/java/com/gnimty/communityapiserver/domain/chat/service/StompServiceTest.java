e01261b2b960bdad01ecaeae696beb8779d8d8b7
janguni <lightson23@naver.com>
2024-01-15 17:47:06 +0900

    chore: 사용하지 않는 import 제거

--- src/test/java/com/gnimty/communityapiserver/domain/chat/service/StompServiceTest.java
+++ src/test/java/com/gnimty/communityapiserver/domain/chat/service/StompServiceTest.java
@@ -36,11 +36,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
 @Slf4j
