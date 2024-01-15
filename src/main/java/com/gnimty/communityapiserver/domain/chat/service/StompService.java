04a32a00cbc6f627d30945b38f1381644b667e5d
janguni <lightson23@naver.com>
2024-01-15 16:38:16 +0900

    bug: 상태변경 로직 버그 수정

--- src/main/java/com/gnimty/communityapiserver/domain/chat/service/StompService.java
+++ src/main/java/com/gnimty/communityapiserver/domain/chat/service/StompService.java
@@ -204,9 +204,11 @@ public class StompService {
 
 	// TODO janguni: 접속정보 변동내역 전송
 	public void updateConnStatus(User user, Status connectStatus, Boolean isByUser) {
-		user.updateNowStatus(connectStatus);
-		if (connectStatus.equals(Status.OFFLINE) && isByUser) {
+		if (isByUser) {
+			user.updateNowStatus(connectStatus);
 			user.updateSelectedStatus(connectStatus);
+		} else {
+			user.updateNowStatus((connectStatus == Status.ONLINE) ? user.getSelectedStatus() : connectStatus);
 		}
 		userService.save(user);
 
