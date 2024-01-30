d8d379cbf676ed2ac24af71ca029fb03d95a0f1b
janguni <lightson23@naver.com>
2024-01-31 00:10:41 +0900

    config: testpropertySource 사용

--- src/test/java/com/gnimty/communityapiserver/domain/chat/service/ChatRoomServiceTest.java
+++ src/test/java/com/gnimty/communityapiserver/domain/chat/service/ChatRoomServiceTest.java
@@ -27,10 +27,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 
 @Slf4j
-@ActiveProfiles(value = "test")
+@TestPropertySource(properties = "spring.mongodb.embedded.version=5.0.6")
 @SpringBootTest
 class ChatRoomServiceTest {
 
