93f771ad05fa4dd07ba4dc085951428614888aeb
janguni <lightson23@naver.com>
2024-02-02 13:11:35 +0900

    config: embedded mongo 자동 설정

--- src/main/java/com/gnimty/communityapiserver/global/config/MongoDBConfiguration.java
+++ src/main/java/com/gnimty/communityapiserver/global/config/MongoDBConfiguration.java
@@ -1,9 +1,15 @@
 package com.gnimty.communityapiserver.global.config;
 
 
+import java.io.IOException;
 import java.util.Arrays;
+
+import com.mongodb.client.MongoClient;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 
 @Configuration
@@ -15,5 +21,15 @@ public class MongoDBConfiguration {
 		return new MongoCustomConversions(
 			Arrays.asList());
 	}
+
+//	@Bean
+//	public MongoTemplate mongoTemplate() throws IOException {
+//
+//		EmbeddedMongoProperties mongo = new EmbeddedMongoFactoryBean();
+//		mongo.setBindIp("localhost");
+//		MongoClient mongoClient = mongo.getObject();
+//		MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, "test_or_whatever_you_want_to_call_this_db");
+//		return mongoTemplate;
+//	}
 
 }
