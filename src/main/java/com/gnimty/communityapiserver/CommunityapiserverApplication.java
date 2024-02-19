5dfe1128e52379d36ee87e4e711468734624ae6e
janguni <lightson23@naver.com>
2024-02-19 11:47:26 +0900

    refactor: timeZone설정

--- src/main/java/com/gnimty/communityapiserver/CommunityapiserverApplication.java
+++ src/main/java/com/gnimty/communityapiserver/CommunityapiserverApplication.java
@@ -1,5 +1,7 @@
 package com.gnimty.communityapiserver;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +12,11 @@ public class CommunityapiserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityapiserverApplication.class, args);
+	}
+
+	@PostConstruct
+	void setUp() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 
